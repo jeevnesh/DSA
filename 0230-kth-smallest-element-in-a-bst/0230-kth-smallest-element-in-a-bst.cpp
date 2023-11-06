7e4039d713e31d24e8785dce25882c8c45a98954
jeevnesh nandwana <jeevneshn@gmail.com>
2023-11-06 16:35:55 +0530

    Time: 17 ms (27.95%), Space: 24.4 MB (48.75%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -10,21 +10,47 @@
  * };
  */
 class Solution {
-    int small(TreeNode* root, int &i, int k)
-    {
-        if(root == NULL)
-            return -1;
-        int left = small(root->left,i,k);
-        if(left != -1)
-            return left;
-        i++;
-        if(i == k)
-            return root->val;
-        return small(root->right,i,k);
-    }
 public:
     int kthSmallest(TreeNode* root, int k) {
-        int i = 0;
-        return small(root,i,k);
+        TreeNode* curr = root;
+        int Ksmallest = 0;
+        //count variable to keep count of visited nodes
+        int count = 0;
+        while(curr != NULL)
+        {
+            //if left is null
+            if(curr->left == NULL)
+            {
+                // first increment count and check if count = k
+                if(++count == k)
+                    Ksmallest = curr->val;
+                // otherwise move to the right child
+                curr = curr->right;
+            }
+            else
+            {
+                // find inorder predeccessor of current Node
+                TreeNode* pred = curr->left;
+                while(pred->right != NULL && pred->right != curr)
+                    pred = pred->right;
+                if(pred->right == NULL)
+                {
+                    // set right child of predeccessor to the current Node
+                    pred->right = curr;
+                    // move current to its left
+                    curr = curr->left;
+                }
+                 // restoring the tree back to original binary
+                // search tree removing threaded links
+                else
+                {
+                    pred->right = NULL;
+                    if(++count == k)
+                        Ksmallest = curr->val;
+                    curr = curr->right;
+                }
+            }  
+        }
+        return Ksmallest;
     }
 };
